37faaa63bdaffc98af7bac2cd7ecfca97845b952
Prachit <prachit.p@codewalla.com>
2023-11-14 20:34:36 +0530

    Initial Commit

---
+++ SwiftUIAzamTut1/SwiftUIAzamTut1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAzamTut1App.swift
+//  SwiftUIAzamTut1
+//
+//  Created by Prachit on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAzamTut1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
