883a77601787bb0167aeec5ced61cdad52ca4b81
Prachit <prachit.p@codewalla.com>
2023-11-23 19:54:05 +0530

    -list background color added

---
+++ SwiftUIAzamTut1/ListBackgroundColor.swift
@@ -0,0 +1,18 @@
+//
+//  ListBackgroundColor.swift
+//  SwiftUIAzamTut1
+//
+//  Created by Prachit on 23/11/23.
+//
+
+import SwiftUI
+
+struct ListBackgroundColor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListBackgroundColor()
+}
