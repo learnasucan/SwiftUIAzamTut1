6b87400dca1c3f6a1ad222638ccbc863d89f2138
Prachit <prachit.p@codewalla.com>
2023-11-23 18:00:03 +0530

    -Integrate group structure

---
+++ SwiftUIAzamTut1/GroupView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupView.swift
+//  SwiftUIAzamTut1
+//
+//  Created by Prachit on 23/11/23.
+//
+
+import SwiftUI
+
+struct GroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupView()
+}
