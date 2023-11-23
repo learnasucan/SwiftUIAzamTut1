ca24c42260f6c64948c67424acd7b2878ab6a398
Prachit <prachit.p@codewalla.com>
2023-11-23 17:14:41 +0530

    -Move cells added

---
+++ SwiftUIAzamTut1/MoveView.swift
@@ -0,0 +1,44 @@
+//
+//  MoveView.swift
+//  SwiftUIAzamTut1
+//
+//  Created by Prachit on 23/11/23.
+//
+
+import SwiftUI
+
+struct MoveView: View {
+    
+    @State var arrayList = ["samir","vishal","shailendra"]
+    
+    private func moveItems(from indices : IndexSet, position toOffset : Int){
+        arrayList.move(fromOffsets: indices, toOffset: toOffset)
+    }
+    
+    var body: some View {
+        NavigationView {
+            
+            VStack {
+                Text("Hello, World!")
+                    .fontDesign(.rounded)
+                    .fontWeight(.bold)
+                List() {
+                    ForEach( arrayList, id: \.self) { name in
+                        Text(name)
+                            .fontDesign(.rounded)
+                            .fontWeight(.bold)
+                    }.onMove(perform: moveItems)
+                }.listStyle(.plain)
+                    .toolbar{
+                        EditButton()
+                    }
+                
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    MoveView()
+}
