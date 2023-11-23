69c0780aa2a9bffa1eddd2e817a151f38420ce6f
Prachit <prachit.p@codewalla.com>
2023-11-23 18:01:14 +0530

    -group view file added

--- SwiftUIAzamTut1/GroupView.swift
+++ SwiftUIAzamTut1/GroupView.swift
@@ -7,9 +7,44 @@
 
 import SwiftUI
 
+struct Post: Identifiable {
+    var id = UUID()
+    var title: String
+    var comments:[Comment] = [Comment]()
+    
+}
+struct Comment: Identifiable {
+    var id = UUID()
+    var comment: String
+}
+
 struct GroupView: View {
+    
+    let posts = [
+        Post(title: "Directions",comments: [
+            Comment(comment: "North"),
+            Comment(comment: "South"),
+            Comment(comment: "West"),
+            Comment(comment: "East"),]),
+        Post(title: "Locations",comments: [
+            Comment(comment: "UP"),
+            Comment(comment: "TamilNadu"),
+            Comment(comment: "Maharashtra"),
+            Comment(comment: "West Bangal")])
+        
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List() {
+            ForEach(posts) { post in
+                Section(header:/*@START_MENU_TOKEN@*/Text(post.title)/*@END_MENU_TOKEN@*/) {
+                    ForEach(post.comments) { comment in
+                        Text(comment.comment)
+                    }
+                }
+            }
+            
+        }.listStyle(.grouped)
     }
 }
 
