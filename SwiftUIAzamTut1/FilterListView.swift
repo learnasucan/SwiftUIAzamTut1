81c818e57c344a1ef65299d89b6598968bc63623
Prachit <prachit.p@codewalla.com>
2023-11-24 00:38:19 +0530

    -searchble logic added

--- SwiftUIAzamTut1/FilterListView.swift
+++ SwiftUIAzamTut1/FilterListView.swift
@@ -13,31 +13,53 @@ struct TitleImage : Identifiable {
     var photo: String
 }
 
+func getPhoto() -> [TitleImage] {
+    return ([TitleImage(name: "sparrow", photo: "sparrow"),
+             TitleImage(name: "snail", photo: "snail"),
+             TitleImage(name: "puma", photo: "puma"),
+             TitleImage(name: "peacock", photo: "peacock"),
+             TitleImage(name: "owl", photo: "owl"),
+             TitleImage(name: "mountain", photo: "mountain"),
+             TitleImage(name: "dragonfly", photo: "dragonfly")])
+}
 
 struct FilterListView: View {
     
-    private func getPhoto() -> [TitleImage] {
-        return ([TitleImage(name: "sparrow", photo: "sparrow"),
-                 TitleImage(name: "snail", photo: "snail"),
-                 TitleImage(name: "puma", photo: "puma"),
-                 TitleImage(name: "peacock", photo: "peacock"),
-                 TitleImage(name: "owl", photo: "owl"),
-                 TitleImage(name: "mountain", photo: "mountain"),
-                 TitleImage(name: "dragonfly", photo: "dragonfly")])
-    }
+    @State var searchName: String = ""
+    
+    
+    @State private var imageList = getPhoto()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        let imageList = getPhoto()
         
-        List(imageList,id: \.id)  { image in
         
-            HStack {
-                Image(image.photo)
-                    .resizable()
-                    .frame(maxWidth: 100,maxHeight: 100)
-                    .clipShape(RoundedRectangle(cornerRadius: 20.0, style: .continuous))
-            }
+        NavigationView {
+            List(imageList,id: \.id)  { image in
+            
+                HStack(alignment: .top) {
+                    Image(image.photo)
+                        .resizable()
+                        .frame(maxWidth: 100,maxHeight: 100)
+                        .clipShape(RoundedRectangle(cornerRadius: 20.0, style: .continuous))
+                    Text(image.name.capitalized)
+                        .fontWeight(.black)
+                        .padding(8.0)
+                    
+                }
+                .padding()
+            }.listStyle(.plain)
+                .searchable(text: $searchName)
+                .onChange(of: searchName) { name in
+                    if !name.isEmpty && name.count >= 1 {
+                        imageList = imageList.filter{
+                            $0.name.lowercased().hasPrefix(searchName.lowercased())
+                        }
+                    } else {
+                        imageList = getPhoto()
+                    }
+                }
+            
+                .navigationTitle("Images")
         }
         
     }
